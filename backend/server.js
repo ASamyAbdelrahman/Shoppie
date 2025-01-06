ab230e6f0ecd70cf44bad964ce45c6e65bda5516
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2025-01-06 19:02:40 +0200

    Ready For Production

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,14 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/products", productRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log("Server is running on port http://localhost:" + PORT);
   connectDB();
